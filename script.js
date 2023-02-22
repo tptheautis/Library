1ddaf48309ffb003e318b0c001ba1b597b8fa523
Theautis <116971196+tptheautis@users.noreply.github.com>
2023-02-22 14:00:52 -0800

    added click even that toggles html form display to block

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ function Book() {
 function addBookToLibrary() {
 
 }
+
+let newBookBtn = document.querySelector('#new-book-btn');
+newBookBtn.addEventListener('click', function() {
+  let newBookForm = document.querySelector('#new-book-form');
+  newBookForm.style.display = 'block';
+});
